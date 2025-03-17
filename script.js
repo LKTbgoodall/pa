77c38a203ca3df01d97f90bbc0dccccbf6fb83cc
LKT <104252528+LKTbgoodall@users.noreply.github.com>
2025-03-17 19:39:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function generatePattern() {
 
   const matricules = {
     matricule497: ":DivisionSAPA: **Gestionnaire PA - 497 | Flora Sancho**",
-    matricule186: ":DivisionSAPA: **Gestionnaire PA - 186 | Alex Mendes**",
+    matricule323: ":DivisionSAPA: **Gestionnaire PA - 323 | Alex Mendes**",
     matricule305: ":DivisionSAPA: **Gestionnaire PA - 305 | Bijou Boubakar**",
     matricule003: ":DivisionSAPA: **Gestionnaire PA - 003 | Yahya Gonzalez**",
     matricule112: ":DivisionSAPA: **Gestionnaire PA - 112 | Adrianna Mendes**",
